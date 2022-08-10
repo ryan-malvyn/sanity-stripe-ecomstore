4c06042c11850e54ce1af0b22c6ba2610223e233
Ryan Malvyn <99323305+ryan-malvyn@users.noreply.github.com>
2022-08-10 17:07:40 +0800

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,7 @@ import { BsBagCheckFill } from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
 import {runFireworks} from '../lib/utils';
 
-const success = () => {
+const Success = () => {
     const {setCartItems, setTotalPrice, setTotalQuantities} = useStateContext();
     const [order,setOrder] = useState(null);
 
@@ -38,4 +38,4 @@ const success = () => {
   )
 }
 
-export default success
+export default Success
